fc4ab04b31905d0f5c0335f93e7b6793b84faa05
se-dducong <diepdc6282@co-well.com.vn>
2019-03-26 14:44:25 +0900

    remove cm9

--- app/src/main/java/com/example/bottomdialogkotlin/MainActivity.kt
+++ app/src/main/java/com/example/bottomdialogkotlin/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // git commit 1
         // git commit 3
+        // git commit 10
     }
     fun cm1(){
 
